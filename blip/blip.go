19ade2624e6f5e438ffd40ff4ed83db75f1549c6
Martin Kalcok <martin.kalcok@gmail.com>
2017-05-03 20:06:40 +0200

    Added configuration via config file

--- blip/blip.go
+++ blip/blip.go
@@ -6,39 +6,79 @@ import (
 	"os"
 	"os/signal"
 	"time"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 )
 
-func stop(mon monitor.Monitor, sig_chan chan os.Signal){
+func stop(monitors []monitor.Monitor, sig_chan chan os.Signal){
 	fmt.Println("Gracefully stopping Blip.")
-	mon.Terminate()
+	for _, mon := range monitors{
+		mon.Terminate()
+	}
+
 	for true{
 		select {
 		case <- sig_chan:
 			fmt.Println("Force stopping Blip")
 			return
 		case <-time.After(100 * time.Millisecond):
-			if !mon.Running(){return}
+			for _, mon := range monitors{
+				if mon.Running(){break}
+			}
+			return
 		}
 	}
 }
 
-func run(){
-	fmt.Println(" Hello world")
-	var host string
-	host = "google.com"
-	mon, err:= monitor.NewPingMonitor(host, 0)
-	if err != nil{
-		panic(fmt.Sprintf("Failed to create Ping Monitor for %s", host))
+func run(blip_conf *monitor.ConfTemplate){
+	var running_monitors []monitor.Monitor
+	for hostname, host_config := range blip_conf.Monitored {
+		if host_config.Ping.Active{
+			ping_conf := host_config.Ping
+			mon, err := monitor.NewPingMonitor(hostname, ping_conf.Interval)
+			if err != nil {
+				panic(fmt.Sprintf("Failed to create Ping Monitor for %s", hostname))
+			}
+			go mon.Run()
+			running_monitors = append(running_monitors, mon)
+		}
 	}
-	mon.SetInterval(15)
-	go mon.Run()
 	sig_chan := make(chan os.Signal, 1)
 	signal.Notify(sig_chan, os.Interrupt)
 	<- sig_chan
-	stop(mon, sig_chan)
+	stop(running_monitors, sig_chan)
 
 }
 
+func parse_conf(path string, dump bool) (cnf *monitor.ConfTemplate){
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil{
+		panic(err)
+	}
+
+	cnf = new(monitor.ConfTemplate)
+
+	err = yaml.Unmarshal(yamlFile, &cnf)
+	if err != nil{
+		panic(err)
+	}
+	if dump {
+		for host, config := range cnf.Monitored {
+			fmt.Printf("Will monitor %s\n", host)
+			if config.Ping.Active {
+				fmt.Println("  Using Ping")
+				fmt.Printf("    Interval: %d s\n", config.Ping.Interval)
+				fmt.Printf("    Warning after %d failed attempts\n", config.Ping.DeadAfter)
+
+			} else {
+				fmt.Println("  Skipping Ping")
+			}
+		}
+
+	}
+	return
+}
 func main(){
-	run()
+	conf := parse_conf("./config.yaml", true)
+	run(conf)
 }
--- monitor/monitor.go
+++ monitor/monitor.go
@@ -18,3 +18,21 @@ type Monitor interface {
 	SetInterval(int) error
 	Running() bool
 }
+
+// Template for unmarshaling of yaml config file
+type ConfTemplate struct{
+	Monitored map[string]confMonitorModules `yaml:"Monitored"` // List of monitored hosts and their parameters
+}
+
+
+// List of modules for specific host
+type confMonitorModules struct{
+	Ping confPingModule `yaml:"ping"`
+}
+
+// Configuration for icmp module
+type confPingModule struct {
+	Active bool `yaml:"active"`
+	Interval int `yaml:"interval"`
+	DeadAfter int `yaml:"deadAfter"`
+}

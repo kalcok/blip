5f9f4480b43cc56519003b1a03af9137030ade4c
Martin Kalcok <martin.kalcok@gmail.com>
2017-05-07 21:25:39 +0200

    Updated config template and example with configuration of Logging module

--- blip/config.go
+++ blip/config.go
@@ -3,6 +3,7 @@ package main
 // Template for unmarshaling of yaml config file
 type ConfTemplate struct{
 	Monitored map[string]confMonitorModules `yaml:"Monitored"` // List of monitored hosts and their parameters
+	Logging confLoggers `yaml:"Logging"`
 }
 
 
@@ -16,4 +17,15 @@ type confPingModule struct {
 	Active bool `yaml:"active"`
 	Interval int `yaml:"interval"`
 	DeadAfter int `yaml:"deadAfter"`
+}
+
+// Configuration for logging modules
+type confLoggers struct{
+	//FileLogger confFileLogger `yaml:"FileLogger"`
+	FileLogger confFileLogger `yaml:"FileLogger"`
+}
+// Configuration for File logger
+type confFileLogger struct{
+	Level string `yaml:"level"`
+	LogFile string `yaml:"logFile"`
 }
